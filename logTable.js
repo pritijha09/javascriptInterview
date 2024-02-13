155ab1e8630c8ab5ecac1e9c278f9ab154ce1972
Priti Kumari <priti.kumari@primussoft.com>
2024-02-13 14:56:16 +0530

    log tabular format

---
+++ logTable.js
@@ -0,0 +1,20 @@
+//console log in tabluar form
+var product = [
+    {
+        name: "Mobile",
+        Price: "$200",
+        id: "01"
+    },
+    {
+        name: "Tv",
+        Price: "$500",
+        id: "02"
+    },
+    {
+        name: "Laptop",
+        Price: "$2000",
+        id: "03"
+    }
+];
+
+console.table(product);
