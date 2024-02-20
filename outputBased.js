7ffbe72fa703dee73aada7d6bbe34433f437dc65
Priti Kumari <priti.kumari@primussoft.com>
2024-02-20 16:29:36 +0530

    push and pop

---
+++ outputBased.js
@@ -0,0 +1,8 @@
+var x=y=z=1;
+//x==y // true = 1
+//1==1
+if(x==y==z){
+    console.log("Hello")
+} else {
+    console.log("hi")
+}
---
+++ pushAndPop.js
@@ -0,0 +1,5 @@
+//Push and Pop in Javascript
+var fruits = ['Banana', 'Orange'];
+fruits.push('Grapes', 'aaa');
+fruits.pop();
+console.log(fruits)
